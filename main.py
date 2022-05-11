7e8d087f162fb4a518ffcaf471c811f3d6166f9b
Tate Deklerk <tdeklerk@student.unimelb.edu.au>
2022-05-11 23:03:04 +1000

    selects movie name from each row, counts total number of movies in list

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 from imdb import Cinemagoer
 import sys, csv
 
+# variables
+MOVIE_COUNT = 0
+
+
 # get the file name
 
 fileName = sys.argv[1]
+
 
 # create an instance of the Cinemagoer class
 ia = Cinemagoer()
 
 # parse CSV file
 
-with open(fileName, 'r') as file:
+file = open(fileName, 'r')
+
+try:
     reader = csv.reader(file)
-    for each_row in reader:
-        print(each_row)
+    for row in reader:
+        movie_name = row[1]
+        # test
+        print(movie_name)
+        MOVIE_COUNT += 1
+
+
+
+
+
+
+
+
+
+
+finally:
+    file.close()
+    print("Total Number of movies watched is", MOVIE_COUNT)
