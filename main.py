7481e5c8f51aaffab223e7d64dd010bb55372530
Tate Deklerk <tdeklerk@student.unimelb.edu.au>
2022-05-11 23:20:35 +1000

    looks up movie name and prints search results

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys, csv
 
 # variables
 MOVIE_COUNT = 0
-
+directors = {}
+actors = {}
 
 # get the file name
-
 fileName = sys.argv[1]
 
 
@@ -19,11 +19,21 @@ file = open(fileName, 'r')
 
 try:
     reader = csv.reader(file)
+    next(file)
     for row in reader:
         movie_name = row[1]
+
+        imdb_movie = ia.search_movie(movie_name)
+
         # test
+        #assert movie_name == imdb_movieName
         print(movie_name)
+        print(imdb_movie)
+        #print(imdb_movie)
+
+        # add 1 to counter
         MOVIE_COUNT += 1
+
 
 
 
